d723ce51ee46391ec0c8d2d92552e860bd2e6671
YashuVerma1210 <verma.yashu32@gmail.com>
2022-06-21 09:48:03 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import { compressed, restored } from './service';
+import { init } from './service_2';
 
 export default function App() {
   const clickHandler = () => {
-    console.log(compressed);
-    console.log(restored);
+    // console.log(compressed);
+    // console.log(restored);
+    init();
   };
 
   return (
---
+++ src/service_2.js
@@ -0,0 +1,41 @@
+import { inflate } from 'pako';
+
+export const urls = [
+  'https://raw.githubusercontent.com/NazarHarashchak/MobileBanking/9155a04a3ff064167537a7c32f9cca356a5c3ab4/FrontEnd/node_modules/.cache/eslint-loader/b3fa51dc9159babf532b97696dacb328bf0a70dc.json.gz',
+  'https://raw.githubusercontent.com/mongodb-university/mflix-python/d9667e709bd400f3d3dbd6e7f1474b3702d9d5fa/data/mflix/comments.metadata.json.gz',
+  'https://raw.githubusercontent.com/dump-sauraj/realme_rmx2185_dump964/3a9c42cac2977a13e43ca8bf1ff886fca730f158/system/system/etc/protolog.conf.json.gz',
+];
+
+export async function exec(i = 0) {
+  console.group('file: ', i);
+  try {
+    // fetch file with CORS enabled
+    const res = await fetch(urls[i], {
+      mode: 'cors',
+    });
+    // convert to arrayBuffer for further processing
+    const buf = await res.arrayBuffer();
+    // or get blob using `await res.blob()`
+    // and convert blob to arrayBuffer using `await blob.arrayBuffer()`
+
+    console.log('input size: ', buf.byteLength);
+
+    // decompress file
+    const outBuf = inflate(buf);
+    console.log('output size: ', outBuf.byteLength);
+
+    // convert arrayBuffer to string
+    const str = new TextDecoder().decode(outBuf);
+    // console.log('json string', str);
+
+    // print json object
+    console.log('json object', JSON.parse(str));
+  } catch (err) {
+    console.error('unable to decompress', err);
+  }
+  console.groupEnd('file: ', i);
+}
+
+export async function init() {
+  for (let i in urls) await exec(i);
+}
